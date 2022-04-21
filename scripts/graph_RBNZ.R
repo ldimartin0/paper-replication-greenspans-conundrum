b071c79b52ba2127fa43d5ae4146336f5b0be351
ldimartin0 <luke.dnu@gmail.com>
2022-04-21 18:43:46 -0400

    Tried new gilt rates, no luck, also updated old graphs to match style

--- scripts/graph_RBNZ.R
+++ scripts/graph_RBNZ.R
@@ -8,7 +8,8 @@ d_fil <- d %>%
 	
 d_chg <- d_fil %>% 
 	select(date, contains("chg")) %>% 
-	pivot_longer(cols = 2:last_col(), names_to = "series")
+	pivot_longer(cols = 2:last_col(), names_to = "series") %>% 
+	mutate(date = as_date(date))
 
 
 ggplot(d_chg, aes(x = date, y = value, linetype = series)) +
@@ -16,5 +17,13 @@ ggplot(d_chg, aes(x = date, y = value, linetype = series)) +
 	ylim(c(-1.5,1)) +
 	scale_linetype_manual(values = c("dashed", "solid")) +
 	labs(title = "New Zealand 10-Year Government Bond Yield and \n RBNZ Cash Rate, March 1999 to May 2012") +
+	scale_x_date(
+		date_breaks = "20 months",
+		labels = scales::label_date("%m-%Y"),
+		expand = c(0,0)) +
 	theme_classic() +
-	theme(panel.grid.major.y = element_line())
+	theme(
+		panel.grid.major.y = element_line(),
+		plot.margin = margin(0, 0, 0, 0, "pt"),
+		axis.line.y = element_blank())
+
